e722477a95dd47bbb224c71d7042f421f401d65d
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-04-15 12:56:39 +0200

    Add nice message for folks who try to activate with PHP less than 5.6

--- sensei-lms-beta.php
+++ sensei-lms-beta.php
@@ -22,6 +22,11 @@ define( 'SENSEI_LMS_BETA_VERSION', '1.0.0-dev' );
 define( 'SENSEI_LMS_BETA_PLUGIN_FILE', __FILE__ );
 define( 'SENSEI_LMS_BETA_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
+if ( version_compare( phpversion(), '5.6.0', '<' ) ) {
+	// translators: %1$s placeholder is minimum PHP version; %2$s is the version of PHP they have installed.
+	die( esc_html( sprintf( __( 'Sensei LMS Beta Tester requires a minimum PHP version of %1$s, but you are running %2$s.', 'sensei-lms-beta' ), '5.6.0', phpversion() ) ) );
+}
+
 // Include deprecated functions.
 require_once dirname( __FILE__ ) . '/includes/class-sensei-lms-beta.php';
 
